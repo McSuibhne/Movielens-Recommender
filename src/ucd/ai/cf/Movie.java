eec9ee22a92258ad686bbd9936b25838dfc4bd46
Jonathan <jonathan.sweeney@ucdconnect.ie>
2019-05-02 14:34:30 -0400

    Initialise Movie class

---
+++ src/ucd/ai/cf/Movie.java
@@ -0,0 +1,15 @@
+package ucd.ai.cf;
+
+
+/**This represents a movie that the users can rate.
+ */
+public class Movie {
+	private String name;
+	private Integer id;
+
+	public Movie(final String name, final Integer id){
+		this.name = name;
+		this.id = id;
+	}
+
+}
